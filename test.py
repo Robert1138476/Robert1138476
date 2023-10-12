5e13097861fe3c87e715efe8c454fb7008925207
Robert1138476 <131493605+Robert1138476@users.noreply.github.com>
2023-10-12 19:10:39 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
-import tkinter as tk
+from colorama import Fore, Back, Style, init
+import sys
+import time
 
-def exit():
-    root.quit()
-    quit()
+init(autoreset=True)
 
-root = tk.Tk()
+def type(text, delay=0.05, style=Fore.WHITE):
+    for char in text:
+        sys.stdout.write(style + char)
+        sys.stdout.flush()
+        time.sleep(delay)
+    print()
 
-button = tk.Button(root, text="hey", command=exit)
-button.pack(expand=True, fill="both")
-
-root.mainloop()
+type("hello", style=Fore.GREEN)
