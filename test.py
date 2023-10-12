c4962e1f4d168bad319368e43cf77663ab958ab2
Robert1138476 <131493605+Robert1138476@users.noreply.github.com>
2023-10-12 18:58:38 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print("hello")
+import tkinter as tk
+
+def exit():
+    root.quit()
+    quit()
+
+root = tk.Tk()
+
+button = tk.Button(root, text="hey", command=exit)
+button.pack(expand=True, fill="both")
+
+root.mainloop()
